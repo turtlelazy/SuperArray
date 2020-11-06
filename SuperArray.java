526e5ac5090ae03f84e120267e86278dc345a25b
Ishraq <ishraqmahid@gmail.com>
2020-11-06 12:18:00 -0500

    lastIndexOf method added

--- SuperArray.java
+++ SuperArray.java
@@ -117,6 +117,15 @@ public class SuperArray{
 		return returnString;
 	}
 	
+	public int lastIndexOf(String value){
+		for(int i = size-1;i>=0;i--){
+			if(get(i).equals(value)){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
 	
 	
 
